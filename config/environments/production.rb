fc95a98f17ff475ca5e87f1c88540c5b6d78f98d
Lyndon McKay <lyndonmckay13@gmail.com>
2017-06-05 17:17:40 -0500

    s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,16 +3,14 @@ Rails.application.configure do
 
   #Amazon Image Saving
   config.paperclip_defaults = {
-          storage: :s3,
-          s3_region: ENV["AWS_REGION"],
-              s3_host_name: "s3-us-west-2.amazonaws.com",
-          s3_credentials: {
-            # s3_host_name: ENV["AWS_HOST_NAME"],
-            bucket: ENV["S3_BUCKET_NAME"],
-            access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-            secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-            }
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
